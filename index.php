f1fed2450f56fc12eceabb7b48db21319a5f5699
zengrijing <zrj>
2020-09-02 09:38:55 +0800

    dddd

--- index.php
+++ index.php
@@ -1,1 +1,42 @@
-3333333333333333331123123
+<?php
+
+class StoreProvider{
+    function __construct()
+    {
+        var_dump('StoreProvider');
+    }
+}
+class ProductProvider{
+    function __construct()
+    {
+        var_dump('ProductProvider');
+    }
+}
+class Test{
+    private $name;
+    public $age;
+    function __construct()
+    {
+        $this['dd']=  $this->index();
+    }
+    function index(){
+        return 'index';
+    }
+}
+
+$test = new Test();
+var_dump($test);exit;
+
+
+
+        $provider = [
+            StoreProvider::class,
+            ProductProvider::class,
+            Test::class,
+            //...其他服务提供者
+        ];
+        $provider_callback = function ($provider) {
+            $obj = new $provider;
+//            $this->serviceRegister($obj);
+        };
+        array_walk($provider, $provider_callback);//注册
